65560a3098b55f8957801569a485df2a14f457be
Kevin Jolley <serrowpls@gmail.com>
2020-09-07 23:15:02 -0500

    dry code, added function to handle repetition

--- scripts.js
+++ scripts.js
@@ -2,12 +2,17 @@ window.addEventListener("load", () => {
   console.log('Working (OvO")');
 });
 
+// Notes
+// Slider is .checked for boolean return
+// Data return is initially going to be done as hidden blocks that are done automatically
+// Future data will only be new data input.
+
 // JS
 
 const form = document.getElementById("text-input");
 const submit = document.getElementById("submit");
-const output = document.getElementById("text-output");
 const error = document.getElementById("error");
+const output = document.getElementById("text-output");
 
 const choiceWrapper = document.getElementsByClassName("choice-item");
 
@@ -15,15 +20,13 @@ const objectChoice = document.getElementById("object-choice");
 const arrayChoice = document.getElementById("array-choice");
 const aobjChoice = document.getElementById("aobj-choice");
 
-// Slider is .checked for boolean return
-
-// -- Two ways to handle data --
-// 1. Data is modified and stored by default, the toggles show/hide the data
-// 2. The data is only modified when the toggles are clicked.
-// First option is going to be the default for now.
+let arr = [];
+let objarr = [];
 
 // Text is always shown by default
 submit.addEventListener("click", () => {
+  // Temporarily placing these in function scope so we can use them to hold the variables.
+
   // Check for no input
   if (!form.value) {
     error.classList.add("shown");
@@ -32,28 +35,48 @@ submit.addEventListener("click", () => {
   }
 
   // Switch case? Has to check the state of 3 different items.. What do I pass for the expression?
+  // If statement block will check them and work like a switch case, works temporarily.
+  if (objectChoice.checked) {
+    ShowObject(objectChoice);
+  }
+  if (arrayChoice.checked) {
+    arr.push(form.value);
+    ShowArray(arr);
+    console.log(arr);
+  }
+  if (aobjChoice.checked) {
+    ShowAobj(aobjChoice);
+  }
 
   // Create list item node and append form value
-  const node = document.createElement("LI");
-  const textNode = document.createTextNode(form.value);
-  // Append node to  output
-  node.appendChild(textNode);
-  output.appendChild(node);
+  CreateNode(form.value, output, "LI");
   // Remove shown class
   error.classList.remove("shown");
   // Clear form value
   form.value = "";
 });
 
+const CreateNode = (value, node, type) => {
+  const output = node;
+  const itemNode = document.createElement(type);
+  const textNode = document.createTextNode(value);
+
+  itemNode.appendChild(textNode);
+  output.appendChild(itemNode);
+};
+
 // Add the input to an object with unique id
 const ShowArray = (input) => {
-  console.log(input);
+  const output = document.getElementById("array-output");
+  CreateNode(input, output, "P");
 };
 // Add the input to an array
 const ShowObject = (input) => {
+  console.log("ShowObject");
   console.log(input);
 };
 // Throw the individual input objects into an array!
 const ShowAobj = (input) => {
+  console.log("ShowAobj");
   console.log(input);
 };
