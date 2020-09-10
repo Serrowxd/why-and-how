b3e66bd3bfcd22ec13747f02666b2d2077c5fcb4
Kevin Jolley <serrowpls@gmail.com>
2020-09-10 04:39:25 -0500

    switch now handles foldout color changes with clear and close

--- web-builder/scripts.js
+++ web-builder/scripts.js
@@ -8,28 +8,57 @@ const pannelSelector = document.getElementById("pannel-items");
 const pannelPop = document.getElementById("populate-pannel");
 const displayPreview = document.getElementById("display-preview");
 
-pannelSelector.addEventListener("click", (event) => {
+const sidePannel = document.getElementById("side-pannel");
+const pannelFold = document.getElementById("pannel-foldout");
+
+sidePannel.addEventListener("click", (event) => {
   // console.log(event.target.getAttribute("id"));
 
   switch (event.target.getAttribute("id")) {
     case "one":
       console.log("one");
-      displayPreview.style.backgroundColor = "red";
+      // displayPreview.style.backgroundColor = "red";
+      sidePannel.classList.add("add-width");
+      pannelFold.classList.add("shown");
       break;
     case "two":
       console.log("two");
-      displayPreview.style.backgroundColor = "blue";
       break;
     case "three":
       console.log("three");
-      displayPreview.style.backgroundColor = "orange";
       break;
     case "four":
       console.log("four");
+      break;
+    case "pf-one":
+      console.log("PF-One Clicked");
+      displayPreview.style.backgroundColor = "red";
+      break;
+    case "pf-two":
+      console.log("PF-One Clicked");
+      displayPreview.style.backgroundColor = "blue";
+      break;
+    case "pf-three":
+      console.log("PF-One Clicked");
+      displayPreview.style.backgroundColor = "orange";
+      break;
+    case "pf-four":
+      console.log("PF-One Clicked");
       displayPreview.style.backgroundColor = "green";
+      break;
+    case "close":
+      sidePannel.classList.remove("add-width");
+      pannelFold.classList.remove("shown");
+      break;
+    case "clear":
+      displayPreview.style.backgroundColor = "inherit";
       break;
     default:
       console.log("oops");
-      displayPreview.style.backgroundColor = "purple";
   }
 });
+
+// Fold-out bar for each selection in initial switch case
+// These contain secondary switch cases that check for click
+// Do something with the preview page on click
+// Add close/return button.
