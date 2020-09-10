620e14431addabe350adb88e02960bf9d69f56fd
Kevin Jolley <serrowpls@gmail.com>
2020-09-10 04:44:45 -0500

    updated console logging, added some notes to the JS file

--- web-builder/scripts.js
+++ web-builder/scripts.js
@@ -5,7 +5,6 @@ window.addEventListener("load", () => {
 // JS
 
 const pannelSelector = document.getElementById("pannel-items");
-const pannelPop = document.getElementById("populate-pannel");
 const displayPreview = document.getElementById("display-preview");
 
 const sidePannel = document.getElementById("side-pannel");
@@ -16,41 +15,43 @@ sidePannel.addEventListener("click", (event) => {
 
   switch (event.target.getAttribute("id")) {
     case "one":
-      console.log("one");
+      console.log("One Clicked");
       // displayPreview.style.backgroundColor = "red";
       sidePannel.classList.add("add-width");
       pannelFold.classList.add("shown");
       break;
     case "two":
-      console.log("two");
+      console.log("Two Clicked");
       break;
     case "three":
-      console.log("three");
+      console.log("Three Clicked");
       break;
     case "four":
-      console.log("four");
+      console.log("Four Clicked");
       break;
     case "pf-one":
       console.log("PF-One Clicked");
       displayPreview.style.backgroundColor = "red";
       break;
     case "pf-two":
-      console.log("PF-One Clicked");
+      console.log("PF-Two Clicked");
       displayPreview.style.backgroundColor = "blue";
       break;
     case "pf-three":
-      console.log("PF-One Clicked");
+      console.log("PF-Three Clicked");
       displayPreview.style.backgroundColor = "orange";
       break;
     case "pf-four":
-      console.log("PF-One Clicked");
+      console.log("PF-Four Clicked");
       displayPreview.style.backgroundColor = "green";
       break;
     case "close":
+      console.log("Close Clicked");
       sidePannel.classList.remove("add-width");
       pannelFold.classList.remove("shown");
       break;
     case "clear":
+      console.log("Clear Clicked");
       displayPreview.style.backgroundColor = "inherit";
       break;
     default:
@@ -58,7 +59,6 @@ sidePannel.addEventListener("click", (event) => {
   }
 });
 
-// Fold-out bar for each selection in initial switch case
-// These contain secondary switch cases that check for click
-// Do something with the preview page on click
-// Add close/return button.
+// This seems like a ridiculously large switch case
+// Text should be included, might make that into a second foldout?
+// Should the foldout items be auto-populated based on what is clicked?
