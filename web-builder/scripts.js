f975f4ee9d254cfec0c9f0021a6b4f3dfb803520
Kevin Jolley <serrowpls@gmail.com>
2020-09-10 04:08:07 -0500

    switch case to change background color

--- web-builder/scripts.js
+++ web-builder/scripts.js
@@ -1,3 +1,35 @@
 window.addEventListener("load", () => {
   console.log('Working (OvO")');
 });
+
+// JS
+
+const pannelSelector = document.getElementById("pannel-items");
+const pannelPop = document.getElementById("populate-pannel");
+const displayPreview = document.getElementById("display-preview");
+
+pannelSelector.addEventListener("click", (event) => {
+  // console.log(event.target.getAttribute("id"));
+
+  switch (event.target.getAttribute("id")) {
+    case "one":
+      console.log("one");
+      displayPreview.style.backgroundColor = "red";
+      break;
+    case "two":
+      console.log("two");
+      displayPreview.style.backgroundColor = "blue";
+      break;
+    case "three":
+      console.log("three");
+      displayPreview.style.backgroundColor = "orange";
+      break;
+    case "four":
+      console.log("four");
+      displayPreview.style.backgroundColor = "green";
+      break;
+    default:
+      console.log("oops");
+      displayPreview.style.backgroundColor = "purple";
+  }
+});
