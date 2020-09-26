e3b627174bcee3d2f5a086bd596256c2b6f0bebc
Kevin Jolley <serrowpls@gmail.com>
2020-09-26 12:51:22 -0500

    new project, sending current stuff before making the new folders

--- web-builder/scripts.js
+++ web-builder/scripts.js
@@ -10,6 +10,12 @@ const displayPreview = document.getElementById("display-preview");
 const sidePannel = document.getElementById("side-pannel");
 const pannelFold = document.getElementById("pannel-foldout");
 
+// State Variables
+let One;
+let Two;
+let Three;
+let Four;
+
 sidePannel.addEventListener("click", (event) => {
   // console.log(event.target.getAttribute("id"));
 
@@ -19,9 +25,13 @@ sidePannel.addEventListener("click", (event) => {
       // displayPreview.style.backgroundColor = "red";
       sidePannel.classList.add("add-width");
       pannelFold.classList.add("shown");
+      Two = false;
+      One = true;
       break;
     case "two":
       console.log("Two Clicked");
+      One = false;
+      Two = true;
       break;
     case "three":
       console.log("Three Clicked");
@@ -30,6 +40,16 @@ sidePannel.addEventListener("click", (event) => {
       console.log("Four Clicked");
       break;
     case "pf-one":
+      if (One) {
+        console.log("Hemlo");
+        displayPreview.style.backgroundColor = "purple";
+        break;
+      }
+      if (Two) {
+        console.log("Not Hemlo >:(");
+        displayPreview.style.backgroundColor = "pink";
+        break;
+      }
       console.log("PF-One Clicked");
       displayPreview.style.backgroundColor = "red";
       break;
@@ -53,6 +73,8 @@ sidePannel.addEventListener("click", (event) => {
     case "clear":
       console.log("Clear Clicked");
       displayPreview.style.backgroundColor = "inherit";
+      One = false;
+      Two = false;
       break;
     default:
       console.log("oops");
@@ -62,3 +84,11 @@ sidePannel.addEventListener("click", (event) => {
 // This seems like a ridiculously large switch case
 // Text should be included, might make that into a second foldout?
 // Should the foldout items be auto-populated based on what is clicked?
+
+// Easiest way to handle this would be to add an if statement to the switch cases.
+// Well set-up a default state via variable, then apply a boolean to it.
+// If that state is met, then we'll pass a certain input to the function call
+// That input wil change the behavior of the PF's.
+
+// Edge cases will be making sure each of the inital buttons sets the other states to false
+// This needs to happen before we set the state for that function.
